b51600b9d795a2ce9e33699dbf2b6d18f20c2b55
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-23 16:48:57 +0100

    Add project files.

---
+++ CalculatorReDux/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator!");
+            //Console.ReadLine();
+
+            Console.WriteLine("Enter one Number: ");
+            string X = Console.ReadLine();
+
+            Console.WriteLine("Enter second Number: ");
+            string Y = Console.ReadLine();
+
+            int FirstInt = int.Parse(X);
+            int SecondInt = int.Parse(Y);
+            int Result = FirstInt * SecondInt;
+
+            Console.WriteLine("The result is: " + Result);
+            Console.ReadLine();
+
+
+        }
+    }
+}
