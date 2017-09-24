0f275ad4045cc13b364d15c299314f73dba913a1
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-24 13:53:53 +0100

    non-working version of calculator accepting unlimited numbers, line 28, input string was not in a correct format.

--- CalculatorReDux/Program.cs
+++ CalculatorReDux/Program.cs
@@ -11,61 +11,46 @@ namespace calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
-            //Console.ReadLine();
-
-            //note that Console.Read only takes a single char.
-            
-
             Console.WriteLine("======================");
+
+            Console.WriteLine("Please enter the desired operator:");
+            String UserOperator = Console.ReadLine();
+
+            //why does it work this way??
+            Console.Write("Please enter numbers " + UserOperator + "? ");
+            //the line below takes the userOperator variable from the user and converts it 
+            int count = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[count];
+            for (int index = 0; index < count; index++)
             {
-                Console.WriteLine("Please enter the desired operator:+");
-                String UserOperator = Console.ReadLine();
-
-                
-                Console.WriteLine("Enter Numbers: ");
-
-                int[] numbers = new int[];
-                string X = Console.ReadLine();
-
-                
-
-                foreach (var item in collection)
-                {
-
-                }
-
-
-                /*
-                Console.WriteLine("Enter second Number: ");
-                string Y = Console.ReadLine();
-
-                int FirstInt = int.Parse(X);
-                int SecondInt = int.Parse(Y);
-
-                int Result = 0;
-                
-                
-                /*
-                if (UserOperator == "+")
-                    Result = FirstInt + SecondInt;
-                else if (UserOperator == "-")
-                    Result = FirstInt - SecondInt;
-                else if (UserOperator == "*")
-                    Result = FirstInt * SecondInt;
-                else if (UserOperator == "/")
-                    Result = FirstInt / SecondInt;
-                /*else
-                {
-                    Result = Result("You didn't chose a valid operator.");
-                }*/
-
-                Console.WriteLine("The result is: " + Result);
-                Console.ReadLine();
+                Console.Write("Please enter number " + (index + 1) + ":");
+                numbers[index] = int.Parse(Console.ReadLine());
             }
 
-            
+            int Result = numbers[0];
 
-
+            for (int index = 1; index < count; index++)
+            {
+                if (UserOperator == "+")
+                {
+                    Result = Result + Result;
+                }
+                else if (UserOperator == "-")
+                {
+                    Result = Result - Result;
+                }
+                else if (UserOperator == "*")
+                {
+                    Result = Result * Result;
+                }
+                else if (UserOperator == "/")
+                {
+                    Result = Result / Result;
+                }
+            }
+            Console.WriteLine("The answer is: " + Result);
+            Console.ReadLine();
         }
     }
 }
