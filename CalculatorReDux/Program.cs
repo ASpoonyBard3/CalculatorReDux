3677130de4edfd8e9b6a6132eb29d296b72542e3
ASpoonyBard3 <Samsonf21@gmail.com>
2017-09-25 16:05:55 +0100

    Add comments to understand control flow

--- CalculatorReDux/Program.cs
+++ CalculatorReDux/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +19,16 @@ namespace Calculator
             Console.Write("How many numbers do you want to " + UserOperator + "? ");
             int Counter = int.Parse(Console.ReadLine());
 
+            //declaring new array numbers as the same size as counter vari
             int[] numbers = new int[Counter];
+
             for (int index = 0; index < Counter; index++)
-            {
-                Console.Write("Please enter number " + (index + 1) + ": ");
-                numbers[index] = int.Parse(Console.ReadLine());
+            {//conditional code
+                Console.Write("Please enter number " + (index + 1) + ": "); // incrementing in the condition and the forloop, why?
+                numbers[index] = int.Parse(Console.ReadLine()); // are we capturing the number of numbers here?
             }
 
-            int answer = numbers[0];
+            int answer = numbers[0]; //declaring new int var, from the start of the numbers arrays 
 
             for (int index = 1; index < Counter; index++)
             {
